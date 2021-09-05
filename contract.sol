d642eda217cae2ca21e1efb03272611d034ccc30
Jefff <jeff@nullus.io>
2021-09-05 17:38:32 +0100

    added solidity contract; added subsuquent frontend integration with contract

---
+++ contract.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// A Very simple Solidity Contract to go along with the frontend
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DrippyDolphins is Ownable {
+    event Mint(address indexed _to, uint256 indexed _tokenId, bytes32 _ipfsHash);
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+
+    uint256 tokenCounter = 1;
+    mapping(uint256 => address) internal idToOwner;
+
+    function mint(address _to, bytes32 _ipfsHash) public {
+        uint256 _tokenId = tokenCounter;
+        idToOwner[_tokenId] = _to;
+        tokenCounter++;
+        emit Mint(_to, _tokenId, _ipfsHash);
+    }
+
+    function transfer(address _to, uint256 _tokenId) public {
+        require(msg.sender == idToOwner[_tokenId]);
+        idToOwner[_tokenId] = _to;
+        emit Transfer(msg.sender, _to, _tokenId);
+    }
+    
+    function withdraw() public onlyOwner {
+        uint balance = address(this).balance;
+        payable(owner()).transfer(balance);
+    }
+}
