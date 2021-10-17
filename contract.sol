f80111475d91cc895cdd84962647c58685efef84
Jefff <jeff@nullus.io>
2021-10-17 19:01:13 +0100

    New Update!

--- contract.sol
+++ contract.sol
@@ -1,188 +1,74 @@
-//SPDX-License-Identifier: <SPDX-License>" to each source file. Use "SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
+/*
+,------.         ,--.                      ,------.         ,--.       ,--.     ,--.                
+|  .-.  \ ,--.--.`--' ,---.  ,---.,--. ,--.|  .-.  \  ,---. |  | ,---. |  ,---. `--',--,--,  ,---.  
+|  |  \  :|  .--',--.| .-. || .-. |\  '  / |  |  \  :| .-. ||  || .-. ||  .-.  |,--.|      \(  .-'  
+|  '--'  /|  |   |  || '-' '| '-' ' \   '  |  '--'  /' '-' '|  || '-' '|  | |  ||  ||  ||  |.-'  `) 
+`-------' `--'   `--'|  |-' |  |-'.-'  /   `-------'  `---' `--'|  |-' `--' `--'`--'`--''--'`----'  
+                     `--'   `--'  `---'                         `--'                                
+
+ - This contract was made by Jefff, as an example for a simple NFT solution, without any fancy gimicks. -
+*/
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract DrippyDolphins is ERC721Enumerable, Ownable {
-    uint256 private basePrice = 99000000000000000; //0.099 ETH
+    using Address for address;
+    using Counters for Counters.Counter;
 
-    uint256 private reserveAtATime = 25;
-    uint256 private reservedCount = 0;
-    uint256 private maxReserveCount = 200;
-    address private OwnerAddress = 0xCA9B2C3B584FC92cE20F0BB260124dF3Ad25Fc43; // Change this to yours.
-
-    string _baseTokenURI;   
+    Counters.Counter private _tokenCount; 
     
-    bool public isActive = false;
-    bool public isAllowListActive = false;
+    /* Initial Values */
+    string public baseURI;
+    bool public saleIsActive = false;
+    uint256 public constant price = 80000000000000000; // 0.080000000000000000 ETH
     
-    uint256 public constant MAX_MINTSUPPLY = 20000; // Max Supply
-    uint256 public maximumAllowedTokensPerPurchase = 20; // Maximum on full release
-    uint256 public allowListMaxMint = 20; // Maximum on presale
-
-    mapping(address => bool) private _allowList;
-    mapping(address => uint256) private _allowListClaimed;
-
-    event AssetMinted(uint256 tokenId, address sender);
-    event SaleActivation(bool isActive);
-
-    constructor(string memory baseURI) ERC721("DrippyDolphins", "GA") {
-        setBaseURI(baseURI);
+    /* Mappings */
+    mapping(address => uint256) public minted; 
+    
+    /* Constructor Arguements- set only on deployment */
+    constructor(string memory baseURl) ERC721("Drippy Dolphins", "DD") {
+        setBaseURI(baseURl);
     }
 
-    modifier saleIsOpen {
-        require(totalSupply() <= MAX_MINTSUPPLY, "Sale has ended.");
-        _;
+    /* Return a Value */
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
     }
 
-    modifier onlyAuthorized() {
-        require(OwnerAddress == msg.sender || owner() == msg.sender);
-        _;
+    /* Set a value */
+    function setBaseURI(string memory __baseURI) public onlyOwner {
+        baseURI = __baseURI;
     }
     
-    function setMaximumAllowedTokens(uint256 _count) public onlyAuthorized {
-        maximumAllowedTokensPerPurchase = _count;
+    function flipSaleState() public onlyOwner {
+        saleIsActive = !saleIsActive;
     }
 
-    function setActive(bool val) public onlyAuthorized {
-        isActive = val;
-        emit SaleActivation(val);
-    }
+    /* Functions */
+    function mint(uint256 _amount) external payable {
+        require(saleIsActive, "Sale must be active");
+        require(!Address.isContract(msg.sender), "Contracts are not allowed to mint");
+        require(minted[msg.sender] + _amount <= 10, "Purchase would exceed max tokens per wallet");
+        require(_tokenCount.current() + _amount <= 10000, "Purchase would exceed max supply of tokens");
+        require(msg.value >= price * _amount, "Ether value sent is not correct");
 
-    function setIsAllowListActive(bool _isAllowListActive) external onlyAuthorized {
-        isAllowListActive = _isAllowListActive;
-    }
 
-    function setAllowListMaxMint(uint256 maxMint) external onlyAuthorized {
-        allowListMaxMint = maxMint;
-    }
-
-    function addToAllowList(address[] calldata addresses) external onlyAuthorized {
-      for (uint256 i = 0; i < addresses.length; i++) {
-        require(addresses[i] != address(0), "Can't add a null address");
-
-        _allowList[addresses[i]] = true;
-        _allowListClaimed[addresses[i]] > 0 ? _allowListClaimed[addresses[i]] : 0;
-      }
-    }
-
-    function checkIfOnAllowList(address addr) external view returns (bool) {
-      return _allowList[addr];
-    }
-
-    function removeFromAllowList(address[] calldata addresses) external onlyAuthorized {
-      for (uint256 i = 0; i < addresses.length; i++) {
-        require(addresses[i] != address(0), "Can't add a null address");
-
-        _allowList[addresses[i]] = false;
-      }
-    }
-
-    function allowListClaimedBy(address owner) external view returns (uint256){
-      require(owner != address(0), 'Zero address not on Allow List');
-
-      return _allowListClaimed[owner];
-    }
-    
-    function setReserveAtATime(uint256 val) public onlyAuthorized {
-        reserveAtATime = val;
-    }
-
-    function setMaxReserve(uint256 val) public onlyAuthorized {
-        maxReserveCount = val;
-    }
-    
-    function setPrice(uint256 _price) public onlyAuthorized {
-        basePrice = _price;
-    }
-    
-    function setBaseURI(string memory baseURI) public onlyAuthorized {
-        _baseTokenURI = baseURI;
-    }
-
-    function getMaximumAllowedTokens() public view onlyAuthorized returns (uint256) {
-        return maximumAllowedTokensPerPurchase;
-    }
-
-    function getPrice() external view returns (uint256) {
-        return basePrice; 
-    }
-
-    function getReserveAtATime() external view returns (uint256) {
-        return reserveAtATime; 
-    }
-
-    function getTotalSupply() external view returns (uint256) {
-        return totalSupply();
-    }
-
-    function getContractOwner() public view returns (address) {    
-        return owner();
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return _baseTokenURI;
-    }
-
-    function reserveNft() public onlyAuthorized {
-        require(reservedCount <= maxReserveCount, "Max Reserves taken already!");
-        uint256 supply = totalSupply();
-        uint256 i;
-        for (i = 0; i < reserveAtATime; i++) {
-            emit AssetMinted(supply + i, msg.sender);
-            _safeMint(msg.sender, supply + i);
-            reservedCount++;
-        }  
-    }
-
-    function mint(address _to, uint256 _count) public payable saleIsOpen {
-        if (msg.sender != owner()) {
-            require(isActive, "Sale is not active currently.");
+        for(uint i; i < _amount; i++) {
+            _tokenCount.increment();
+            _safeMint(msg.sender, _tokenCount.current());
         }
-        
-        require(totalSupply() + _count <= MAX_MINTSUPPLY, "Total supply exceeded.");
-        require(totalSupply() <= MAX_MINTSUPPLY, "Total supply spent.");
-        require(
-            _count <= maximumAllowedTokensPerPurchase,
-            "Exceeds maximum allowed tokens"
-        );
-        require(msg.value >= basePrice * _count, "Insuffient ETH amount sent.");
 
-        for (uint256 i = 0; i < _count; i++) {
-            emit AssetMinted(totalSupply(), _to);
-            _safeMint(_to, totalSupply());
-        }
+        minted[msg.sender] += _amount;
+    }
+    
+    /* withdraw */
+    function withdraw() external onlyOwner {
+        uint balance = address(this).balance;
+        payable(msg.sender).transfer(balance);
     }
 
-  function preSaleMint(uint256 _count) public payable saleIsOpen {
-    require(isAllowListActive, 'Allow List is not active');
-    require(_allowList[msg.sender], 'You are not on the Allow List');
-    require(totalSupply() < MAX_MINTSUPPLY, 'All tokens have been minted');
-    require(_count <= allowListMaxMint, 'Cannot purchase this many tokens');
-    require(_allowListClaimed[msg.sender] + _count <= allowListMaxMint, 'Purchase exceeds max allowed');
-    require(msg.value >= basePrice * _count, 'Insuffient ETH amount sent.');
-
-    for (uint256 i = 0; i < _count; i++) {
-      _allowListClaimed[msg.sender] += 1;
-      emit AssetMinted(totalSupply(), msg.sender);
-      _safeMint(msg.sender, totalSupply());
-    }
-  }
-  
-function walletOfOwner(address _owner) external view returns(uint256[] memory) {
-    uint tokenCount = balanceOf(_owner);
-    uint256[] memory tokensId = new uint256[](tokenCount);
-    for(uint i = 0; i < tokenCount; i++){
-        tokensId[i] = tokenOfOwnerByIndex(_owner, i);
-    }
-    return tokensId;
-  }
-
-  function withdraw() external onlyAuthorized {
-    payable(owner()).transfer(address(this).balance);
-  }
 }
---
+++ create.php
@@ -0,0 +1,199 @@
+<?php
+require 'vendor/autoload.php';
+
+use Gregwar\Image\Image;
+
+$typeArr = array(
+	"Alien Gull" => [0, 3],
+	"Ape Gull" => [3, 7],
+	"Blue Gull" => [7, 17],
+	"Golden Gull" => [17, 18],
+	"Green Gull" => [18, 28],
+	"Gull" => [28, 83],
+	"Pink Gull" => [83, 93],
+	"Steel Gull" => [93, 95],
+	"Zombie Gull" => [95, 100]
+);
+
+$accessoryArr = array(
+	"007" => [0, 1.85],
+	"Banana" => [1.85, 3.35],
+	"Black Bandana" => [3.35, 6.8],
+	"Ciggarette" => [6.8, 9.7],
+	"Covid" => [9.7, 12.4],
+	"Fries Lover" => [12.4, 14.6],
+	"Gangsta" => [14.6, 19.35],
+	"Gentleman" => [19.35, 22.65],
+	"Guiltless" => [22.65, 24.35],
+	"Hawaiian Mood" => [24.35, 27.35],
+	"Lifebuoy" => [27.35, 30.5],
+	"Pink Pendant" => [30.5, 35.1],
+	"Red Bandana" => [35.1, 38.55],
+	"Reporter" => [38.55, 43.05],
+	"Rich Gentleman" => [43.05, 46.3],
+	"Riddle Solver" => [46.3, 48.7],
+	"Scarf" => [48.7, 53.25],
+	"True Elegant" => [53.25, 55.85],
+	"Wizard Gem" => [55.85, 58.35],
+	"None" => [58.35, 100]
+);
+
+$eyesArr = array(
+	"3D Glasses" => [0, 4.1],
+	"CSI Glasses" => [4.1, 9.45],
+	"Climber Glasses" => [9.45, 14.55],
+	"CyberPunk" => [14.55, 17.95],
+	"Extermination" => [17.95, 20.35],
+	"Fashionista" => [20.35, 24.6],
+	"Gimmie Your Wallet" => [24.6, 27.4],
+	"Joker" => [27.4, 30], "Nerd" => [30, 35.2],
+	"Night Vision" => [35.2, 40.2],
+	"Over 9000" => [40.2, 42],
+	"Romantic" => [42, 45],
+	"Sad Boi" => [45, 47.9],
+	"Scar" => [47.9, 51.1],
+	"Snorkeling Mask" => [51.1, 56.5],
+	"Special Agent Glasses" => [56.5, 61.75],
+	"Strength Mark" => [61.75, 65.25],
+	"Swimming Goggles" => [65.25, 70.65],
+	"True Fashionista" => [70.65, 74.9],
+	"Wisdom Mark" => [74.9, 78.9],
+	"None" => [78.9, 100]
+);
+
+$hatArr = array(
+	"Angel" => [0, 2],
+	"Bad Trip" => [2, 3.4],
+	"Beret" => [3.4, 6.9],
+	"Black Baseball Cap" => [6.9, 10.15],
+	"Blue Baseball Cap" => [10.15, 13.4],
+	"Blue Beanie" => [13.4, 16.75],
+	"Buccaneer" => [16.75, 19.45],
+	"Burger King" => [19.45, 21.6],
+	"Candlestick" => [21.6, 24.05],
+	"Catch Em All" => [24.05, 25.8],
+	"Cowboy" => [25.8, 29.4],
+	"Deckhand" => [29.4, 32.2],
+	"Devil" => [32.3, 34.15],
+	"Franchise King" => [34.15, 35.25],
+	"General" => [35.25, 36.9],
+	"Good Trip" => [36.9, 38.4],
+	"Green Headphones" => [38.4, 41.85],
+	"Imperator" => [41.85, 43.55],
+	"Jester" => [43.55, 46.25],
+	"King Bong" => [46.25, 48.6],
+	"McDonalds Employee" => [48.6, 51.55],
+	"Pharaoh" => [51.55, 52.55],
+	"Police Officer" => [52.55, 55.65],
+	"Purple Baseball Cap" => [55.65, 58.9],
+	"Red Beanie" => [58.9, 62.25],
+	"Red Headphones" => [62.25, 65.65],
+	"Rice Collector" => [65.65, 68.3],
+	"Ross Afro" => [68.3, 71.3],
+	"Sad Past" => [71.3, 73.1],
+	"Sanchez" => [73.1, 74.95],
+	"Thief Cap" => [74.95, 78.45],
+	"To The Moon" => [78.45, 79.25],
+	"Viking" => [79.25, 82.75],
+	"Zombie" => [82.75, 83.65],
+	"None" => [83.65, 100]
+);
+$eyesPerc = rand(1, 999) / 10;
+$typePerc = rand(1, 999) / 10;
+$accessoryPerc = rand(1, 999) / 10;
+$hatPerc = rand(1, 999) / 10;
+$type = "";
+$eyes = "";
+$accessory = "";
+$hat = "";
+$hash = ""; // tmp
+
+foreach ($typeArr as $x => $y) {
+	if ($typePerc >= $y[0] && $typePerc < $y[1]) {
+		$type = $x;
+		$hash = $hash . sprintf("%02d", array_search($type, array_keys($typeArr)));
+		break;
+	}
+}
+foreach ($accessoryArr as $x => $y) {
+	if ($accessoryPerc >= $y[0] && $accessoryPerc < $y[1]) {
+		$accessory = $x;
+		$hash = $hash . sprintf("%02d", array_search($accessory, array_keys($accessoryArr)));
+		break;
+	}
+}
+foreach ($eyesArr as $x => $y) {
+	if ($eyesPerc >= $y[0] && $eyesPerc < $y[1]) {
+		$eyes = $x;
+		$hash = $hash . sprintf("%02d", array_search($eyes, array_keys($eyesArr)));
+		break;
+	}
+}
+foreach ($hatArr as $x => $y) {
+	if ($hatPerc >= $y[0] && $hatPerc < $y[1]) {
+		$hat = $x;
+		$hash = $hash . sprintf("%02d", array_search($hat, array_keys($hatArr)));
+		break;
+	}
+}
+
+
+$hash = addslashes($_POST["supply"]);
+$roll = $typePerc . "  " . $accessoryPerc . "  " . $eyesPerc . "  " . $hatPerc;
+$attributes = [
+	["Unique Hash" => $hash],
+	["Type" => $type],
+	["Accessory" => $accessory],
+	["Eyes" => $eyes],
+	["Hat" => $hat],
+	["Roll" => $roll]
+];
+
+$metadata = array(
+	"Name" => "CryptoGull #" . $hash,
+	"Description" => "CryptoGulls is an example NFT project.",
+	"image" => "https://jefff000.github.io/nft-generation/meta/" . $hash,
+	"external_url" => "https://jefff000.github.io/nft-generation",
+	"attributes" => [
+		[
+			"trait_type" => "Type",
+			"value" => $type,
+		],
+		[
+			"trait_type" => "Accessory",
+			"value" => $accessory,
+		],
+		[
+			"trait_type" => "Eyes",
+			"value" => $eyes,
+		],
+		[
+			"trait_type" => "Hat",
+			"value" => $hat,
+		],
+		[
+			"trait_type" => "Roll",
+			"value" => $roll,
+		]
+	]
+);
+
+if (!file_exists("meta/" . $hash . ".png")) {
+	Image::open('layers/Background/Background.png')
+		->merge(Image::open('layers/Type/' . $type . '.png'))
+		->merge(Image::open('layers/Accessory/' . $accessory . '.png'))
+		->merge(Image::open('layers/Eyes/' . $eyes . '.png'))
+		->merge(Image::open('layers/Hat/' . $hat . '.png'))
+		->save("meta/" . $hash . ".png", 'png');
+
+	// Add our metadata
+	$fp = fopen("meta/" . $hash, 'wb');
+	fwrite($fp, json_encode($metadata, JSON_PRETTY_PRINT));
+	fclose($fp);
+}
+
+$res = new StdClass();
+$res->url = "meta/" . $hash . ".png";
+$res->attributes = $attributes;
+
+die(json_encode($res, JSON_PRETTY_PRINT));
